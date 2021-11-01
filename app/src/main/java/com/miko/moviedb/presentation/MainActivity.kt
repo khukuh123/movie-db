c7722d292f7cb8684a7311613cf4084524690227
khukuh123 <khukuh.prihatmikho2@gmail.com>
2021-11-02 05:11:39 +0700

    init-project -add mainactivity and detail page

---
+++ app/src/main/java/com/miko/moviedb/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.miko.moviedb.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.miko.moviedb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+}
---
+++ app/src/main/java/com/miko/moviedb/presentation/detail/DetailActivity.kt
@@ -0,0 +1,57 @@
+package com.miko.moviedb.presentation.detail
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
+import com.miko.moviedb.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private var binding: ActivityDetailBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        initUI()
+    }
+
+    private fun initUI(){
+        binding?.let { b ->
+            setSupportActionBar(b.tbDetail)
+            supportActionBar?.title = ""
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            var isCollapsed = false
+            var scrollRange = -1
+            b.ablDetail.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout?.totalScrollRange!!
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    b.ctbDetail.title = "Now You See Me 2"
+                    isCollapsed = true
+                }else if(isCollapsed) {
+                    b.ctbDetail.title = ""
+                    isCollapsed = false
+                }
+            })
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+}
